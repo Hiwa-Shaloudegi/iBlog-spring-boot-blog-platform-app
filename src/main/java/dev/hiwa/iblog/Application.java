03ade799585076cd8e4be240b9be4125d0632fd6
Hiwa Shalouedgi <hiwa.shaloudegi@gmail.com>
2025-07-02 21:55:57 +0330

    init commit

---
+++ src/main/java/dev/hiwa/iblog/Application.java
@@ -0,0 +1,13 @@
+package dev.hiwa.iblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/test/java/dev/hiwa/iblog/ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.hiwa.iblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
